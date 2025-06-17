0accac388cc7d1f57e195d4d9bfa2a01949c652b
AbraaoDev <abraaohomualdo@gmail.com>
2025-06-17 10:06:05 -0300

    feat: added logger for Db

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -4,33 +4,62 @@ import (
 	"echocrud/internal/entities"
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
+
 func NewPostgresConnection() *gorm.DB {
-    dbHost := "localhost"
-    dbPort := "5432"
-    dbUser := "docker"
-    dbPassword := "docker"
-    dbName := "echocrud"
+	dbHost := "localhost"
+	dbPort := "5432"
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbName := "echocrud"
 
-    dsn := fmt.Sprintf(
-        "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Sao_Paulo",
-        dbHost, dbUser, dbPassword, dbName, dbPort,
-    )
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Sao_Paulo",
+		dbHost, dbUser, dbPassword, dbName, dbPort,
+	)
 
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatalf("failed to connect to database: %v", err)
-    }
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), 
+		logger.Config{
+			SlowThreshold:             time.Second,   
+			LogLevel:                  logger.Info,   
+			IgnoreRecordNotFoundError: true,          
+			ParameterizedQueries:      false,        
+			Colorful:                  true,         
+		},
+	)
 
-    // Auto Migrate
-    err = db.AutoMigrate(&entities.Establishment{}, &entities.Store{})
-    if err != nil {
-        log.Fatalf("failed to migrate database: %v", err)
-    }
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
 
-    return db
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("failed to get database instance: %v", err)
+	}
+
+	err = sqlDB.Ping()
+	if err != nil {
+		log.Fatalf("failed to ping database: %v", err)
+	}
+
+	// Auto Migrate
+	log.Println("🔄 Running database migrations...")
+	err = db.AutoMigrate(&entities.Establishment{}, &entities.Store{})
+	if err != nil {
+		log.Fatalf("failed to migrate database: %v", err)
+	}
+	log.Println("✅ Database migrations completed!")
+
+	return db
 }
